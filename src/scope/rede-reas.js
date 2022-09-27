ba854a77ea747345e7d0386bda669d595d2884e8
fdcorrea12 <fdcorrea12@hotmail.com>
2022-09-27 11:45:39 -0500

    Curso-scope-closure-finally

---
+++ src/scope/rede-reas.js
@@ -0,0 +1,30 @@
+var fisrtName; //declarando y es undefined
+fisrtName ='oscar'; //Asignacion
+console.log(fisrtName);
+
+var lastName ='David';//asignado y declarando
+lastName = 'Ana'; //reasingnar
+console.log(lastName)
+
+var secondName ='David'; //declarando y asignando 
+var secondName ='Ana'; //reasignando
+console.log(secondName);
+
+// let fruit ='apple'; //declara y asignar
+fruit= 'kiwi'; //reasignar
+console.log(fruit)
+
+let fruit ='Banana';
+console.log(fruit)
+
+const animal ='dog'; // declaracion y asignar
+// animal='cat';// reasignación
+console.log(animal);
+
+
+const vehicles = [];
+vehicles.push("carrito");
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles)
---
+++ src/scope/strict.js
@@ -0,0 +1,10 @@
+'use strict';
+
+pi =3.1416;
+console.log(pi);
+
+function myFunction (){
+    'use strict';
+    return pi= 312;
+}
+console.log(myFunction());
